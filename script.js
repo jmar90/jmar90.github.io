e740c6ca4aa7b21cbdfb39e6184c7878d1a77ae6
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-03-07 21:35:27 -0500

    Additional updates to header

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// Set up TypeIt script
 new TypeIt('#hero-header', {
   strings: ["Hi, I'm Julia.",  "A full stack web developer."],
-  speed: 50,
+  speed: 60,
   waitUntilVisible: true
 }).go();
+
+// Smooth scrolling when clicking on a tags
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
