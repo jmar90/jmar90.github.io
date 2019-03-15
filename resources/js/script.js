18d63da08258fc8f093f0ae390374946b69354df
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-03-14 22:40:55 -0400

    Added CSS media queries

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,16 @@
-// Set up TypeIt script
+////////////////////////////////
+// TYPE IT SCRIPT FOR HEADER //
+///////////////////////////////
 new TypeIt('#hero-header', {
   strings: ["Hi, I'm Julia.",  "A full stack web developer."],
   speed: 60,
   waitUntilVisible: true
 }).go();
 
+
+////////////////////////////////
+//      DOM MANIPULATION     //
+///////////////////////////////
 // Smooth scrolling when clicking on anchor tags
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (event) {
@@ -17,3 +23,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+// Click event listener for mobile nav
