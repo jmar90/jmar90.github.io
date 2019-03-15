a4252645f08b003bf20daaea2a08df5b57fff707
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-03-15 15:24:50 -0400

    Added mobile navigation menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -23,4 +23,41 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Click event listener for mobile nav
+// If someone clicks on hamburger menu icon, show mobile nav
+const icon = document.querySelector('.mobile-nav-icon i');
+const nav = document.querySelector('#nav');
+const navIconComb = document.querySelector('nav .row');
+const header = document.querySelector('header');
+
+const mobileNav = () => {
+  if(icon.classList.contains('fa-bars') && nav.classList.contains('flex-nav')){
+    icon.classList.add('fa-times');
+    icon.classList.remove('fa-bars');
+    nav.classList.add('mobile-nav');
+    nav.classList.remove('flex-nav');
+  } else {
+    icon.classList.add('fa-bars');
+    icon.classList.remove('fa-times');
+    nav.classList.add('flex-nav');
+    nav.classList.remove('mobile-nav');
+  }
+}
+
+icon.addEventListener('click', mobileNav);
+
+// If someone clicks outside navbar/icon OR scrolls outside of header area, close nav menu
+const closeMobileNav = () => {
+  nav.classList.add('flex-nav');
+  nav.classList.remove('mobile-nav');
+  icon.classList.add('fa-bars');
+  icon.classList.remove('fa-times');
+}
+
+document.addEventListener('click', function(event) {
+  let isClickInside = navIconComb.contains(event.target);
+  if(!isClickInside){
+    closeMobileNav();
+  }
+})
+
+header.addEventListener('mouseleave', closeMobileNav);
